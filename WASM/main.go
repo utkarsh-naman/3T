790e9d6abfc5aa30144b1e610361036bd2db0cfc
Utkarsh Naman <truly.away@gmail.com>
2026-01-23 19:49:19 +0530

    updated WASM

--- WASM/main.go
+++ WASM/main.go
@@ -1,34 +1,19 @@
 package main
 
-import (
-	"syscall/js"
-)
+import "syscall/js"
 
-func main() {
-	// Create a channel to prevent the Go program from exiting immediately.
-	// WASM modules need to stay "running" to listen for JS calls.
-	c := make(chan struct{})
-
-	// Register the function on the global 'window' object (js.Global())
-	js.Global().Set("TTTNGinPlay", js.FuncOf(wrapperTTTNGinPlay))
-
-	// Block forever so the WASM module stays active
-	<-c
-}
-
-func wrapperTTTNGinPlay(_ js.Value, args []js.Value) interface{} {
-	if len(args) == 0 {
-		return "Error: No state provided"
+func tttNginPlayWrapper(_ js.Value, args []js.Value) any {
+	if len(args) != 1 {
+		return js.ValueOf("Expected 1 argument: state (uint32)")
 	}
 
-	// 1. Get the input (State) from the first JavaScript argument
-	// We use .Int() to get the number, then cast to your State type (uint32).
-	inputVal := args[0].Int()
-	inputState := State(inputVal)
+	state := State(uint32(args[0].Int()))
+	move := TTTNGinPlay(state)
 
-	// 2. Call your engine function
-	resultMove := TTTNGinPlay(inputState)
+	return js.ValueOf(int(move))
+}
 
-	// 3. Return the result back to JavaScript
-	return int(resultMove)
+func main() {
+	js.Global().Set("TTTNGinPlay", js.FuncOf(tttNginPlayWrapper))
+	select {} // keep WASM alive
 }
